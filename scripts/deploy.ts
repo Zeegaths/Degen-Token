5b9bac9af8f6094f6bff3643ec4747c4c74c878b
Zeegaths <zarahgathoni76@gmail.com>
2024-02-25 21:06:07 +0100

    degen

---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  const degenGame = await ethers.deployContract("DegenGame");
+
+  await degenGame.waitForDeployment();
+
+  console.log(
+    `DegenGame was deployed to ${degenGame.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
